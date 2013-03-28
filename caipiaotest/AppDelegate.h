eb4314a677ccc1529aef8a61eab18c5c84f10e7e
yuanda <yuanda@localhost.(none)>
2013-03-28 10:36:10 +0800

    Initial Commit

---
+++ caipiaotest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  caipiaotest
+//
+//  Created by yuanda on 13-3-28.
+//  Copyright (c) 2013年 wangjunjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
